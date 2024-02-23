4c1c6e1b68f667dde06d3ca63f10d5f7a7996647
Claudia <claudiabarcelovaldes40@gmail.com>
2024-02-23 16:09:55 +0100

    feat: fix the mappings

--- orgSubgraph/src/org-manager.ts
+++ orgSubgraph/src/org-manager.ts
@@ -1,56 +1,55 @@
 import {
-  MemberAddedToOrganization as MemberAddedToOrganizationEvent,
+  MemberJoinedOrganization as MemberJoinedOrganizationEvent,
+  MemberLeavedOrganization as MemberLeavedOrganizationEvent,
+  VotingPowerSetToMember as VotingPowerSetToMemberEvent,
   MemberCreated as MemberCreatedEvent,
   OrganizationCreated as OrganizationCreatedEvent
 } from "../generated/OrgManager/OrgManager"
 import {
-  MemberAddedToOrganization,
-  MemberCreated,
-  OrganizationCreated
+  Member,
+  Organization,
+  MemberOrganization,
+  VotingPower
 } from "../generated/schema"
 
-export function handleMemberAddedToOrganization(
-  event: MemberAddedToOrganizationEvent
+export function handleMemberJoinedOrganization(
+  event: MemberJoinedOrganizationEvent
 ): void {
-  let entity = new MemberAddedToOrganization(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.orgId = event.params.orgId
-  entity.memberId = event.params.memberId
+  let memberOrg = new MemberOrganization([event.params.orgId.toString(),event.params.memberId.toString() ].join("-"))
+  memberOrg.organization = event.params.orgId.toString()
+  memberOrg.member = event.params.memberId.toString()
+   memberOrg.save()
+}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+export function handleMemberLeavedOrganization(
+  event: MemberLeavedOrganizationEvent
+): void {
+  let memberOrg = new MemberOrganization([event.params.orgId.toString(),event.params.memberId.toString() ].join("-"))
+  memberOrg.organization = event.params.orgId.toString()
+  memberOrg.member = event.params.memberId.toString()
+   memberOrg.save()
+}
 
-  entity.save()
+export function handleVotingPowerSetToMember(
+  event: VotingPowerSetToMemberEvent
+): void {
+  let votingPower = new VotingPower([event.params.orgId.toString(),event.params.memberId.toString() ].join("-"))
+  votingPower.value = event.params.votingPower
+  votingPower.member = event.params.memberId.toString()
+  votingPower.organization = event.params.orgId.toString()
+  votingPower.save()
 }
 
 export function handleMemberCreated(event: MemberCreatedEvent): void {
-  let entity = new MemberCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.memberId = event.params.memberId
-  entity.name = event.params.name
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let member = new Member(event.params.memberId.toString())
+  member.name = event.params.name
+  member.save()
 }
 
 export function handleOrganizationCreated(
   event: OrganizationCreatedEvent
 ): void {
-  let entity = new OrganizationCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.orgId = event.params.orgId
-  entity.name = event.params.name
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let organization = new Organization(event.params.orgId.toString())
+  organization.name = event.params.name
+  organization.save()
 }
