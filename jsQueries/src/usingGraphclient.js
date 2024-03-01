8e062f3cf665d794899fd3e5e1892e29c7eee7a2
Claudia <claudiabarcelovaldes40@gmail.com>
2024-03-01 21:10:21 +0100

    fix: query

--- jsQueries/src/usingGraphclient.js
+++ jsQueries/src/usingGraphclient.js
@@ -1,6 +1,5 @@
 // fetches the data from the subgraph using graphclient
 const graphClient = require('../.graphclient/index.js');
-const { AllDelegators } = require('./queries/members.gql');
 const { membersQuery, delegatorQuery } = require('./queries/queries.js');
 
 // the .graphclientrc.yml file is used to configure the graphclient
@@ -8,7 +7,7 @@ const { membersQuery, delegatorQuery } = require('./queries/queries.js');
 
 async function getQueryData() {
   //getting member data
-  const membersResult = await graphClient.execute(AllDelegators, {});
+  const membersResult = await graphClient.execute(membersQuery, {});
   console.log(membersResult.data.members);
   //getting token data
   const delegatorResult = await graphClient.execute(delegatorQuery, {});
